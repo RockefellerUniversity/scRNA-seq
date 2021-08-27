582e1a84630c0c21009c81ed0619101b6fadd17e
root <tc.infomatics@gmail.com>
2021-08-27 15:28:48 +0000

    Autobuild

--- docs/presentations/r_code/Session1.R
+++ docs/presentations/r_code/Session1.R
@@ -33,35 +33,35 @@ if(params$isSlides == "yes"){
 knitr::include_graphics("imgs/cell_count.png")
 
 
-## ----scSeq_GI_sessionInfot,echo=FALSE,out.width = "50%",fig.align="center"----
+## ----scSeq_GI_sessionInfot,echo=FALSE,out.width = "75%",fig.align="center"----
 knitr::include_graphics("imgs/session_info.png")
 
 
-## ----scSeq_GI_seqQC,echo=FALSE,out.width = "50%",fig.align="center"-----------
+## ----scSeq_GI_seqQC,echo=FALSE,out.width = "75%",fig.align="center"-----------
 knitr::include_graphics("imgs/seq_qc.png")
 
 
-## ----scSeq_GI_mapQC,echo=FALSE,out.width = "50%",fig.align="center"-----------
+## ----scSeq_GI_mapQC,echo=FALSE,out.width = "75%",fig.align="center"-----------
 knitr::include_graphics("imgs/map_info.png")
 
 
-## ----scSeq_GI_cellCount2,echo=FALSE,out.width = "50%",fig.align="center"------
+## ----scSeq_GI_cellCount2,echo=FALSE,out.width = "75%",fig.align="center"------
 knitr::include_graphics("imgs/cell_count.png")
 
 
-## ----scSeq_GI_detCellNum,echo=FALSE,out.width = "25%",fig.align="center"------
+## ----scSeq_GI_detCellNum,echo=FALSE,out.width = "50%",fig.align="center"------
 knitr::include_graphics("imgs/knee_plot.png")
 
 
-## ----scSeq_GI_geneEvl,echo=FALSE,out.width = "50%",fig.align="center"---------
+## ----scSeq_GI_geneEvl,echo=FALSE,out.width = "75%",fig.align="center"---------
 knitr::include_graphics("imgs/gene_eval.png")
 
 
-## ----scSeq_GI_satPlot,echo=FALSE,out.width = "25%",fig.align="center"---------
+## ----scSeq_GI_satPlot,echo=FALSE,out.width = "50%",fig.align="center"---------
 knitr::include_graphics("imgs/umi_eval.png")
 
 
-## ----scSeq_GI_tsnePlot,echo=FALSE,out.width = "75%",fig.align="center"--------
+## ----scSeq_GI_tsnePlot,echo=FALSE,out.width = "100%",fig.align="center"-------
 knitr::include_graphics("imgs/tsne.png")
 
 
